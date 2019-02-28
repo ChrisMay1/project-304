5e177b7167ff160b2786f38f10e969ad3df38e67
ChrisMay1 <maychr@oregonstate.edu>
2019-02-27 22:16:36 -0800

    Cleaning House

---
+++ Trial Scripts/another_test.py
@@ -0,0 +1,12 @@
+class puppy:
+
+	def __init__(self, name, breed):
+		self.name = name
+		self.breed = breed
+
+	def print_breed(self):
+		print(self.breed)
+
+	def print_name(self):
+		print(self.name)
+
