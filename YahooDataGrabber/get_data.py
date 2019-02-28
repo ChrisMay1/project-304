9735215f2dfe8808586cbbab6e6ec64736c07f7b
Roo541 <33165603+Roo541@users.noreply.github.com>
2019-02-27 23:08:34 -0800

    Added how to pull basic finance Data with Yahoo API

--- YahooDataGrabber/get_data.py
+++ YahooDataGrabber/get_data.py
@@ -1,3 +1,19 @@
-import yahoo-finance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+import pandas as pd
+import pandas_datareader.data as web
+from matplotlib import style
+
+style.use('ggplot')
+
+start = dt.datetime(2018, 01, 01)
+end = dt.datetime(2019, 02, 27)
+
+ticker = 'INTC'
+
+df = web.DataReader(ticker, 'yahoo', start, end)
+
+print df.tail()
 
 print("Done")
