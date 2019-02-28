c9723f8f792173fef5a9de1aba6f47d584320dd1
Roo541 <33165603+Roo541@users.noreply.github.com>
2019-02-28 15:22:39 -0800

    now looks a bit more like monte carlo

--- Casino/roullette.py
+++ Casino/roullette.py
@@ -3,24 +3,27 @@ import math
 import random as rand
 import matplotlib.pyplot as plt
 
-period = 1000
-times = 1000
-array = [1]*times
-bank = 1000.0
+period = 100
+times = 100
+initial = 100.0
+bank = initial
 results = []*period
 bet = 5.0
 j = 0 
 
 while j <= period:
+	array = [1]*times
 	for i in range(times):
 		array[i] = rand.randint(0,37)
-	print j, 'Round'
+	# ~ print j, 'Round'
 	a = 0.0
 	b = 0.0
 	c = 0.0
 	d = 0.0
 	
 	for i in range(len(array)):
+		if bank <= 0:
+			break
 		if array[i] >= 1 and array[i] <= 12:		#win
 			a = a + 1
 			bank = bank + bet 
@@ -34,9 +37,22 @@ while j <= period:
 			bank = bank - 2*bet
 			d = d + 1
 	results.append(bank)
-	bank = 100.0
+	bank = initial 
 	j = j + 1
 
+positive = 0.0
+negative = 0.0
+
+for i in range(len(results)):
+	if results[i] > initial:
+		positive = positive + 1
+	else:
+		negative = negative + 1
+		
+print positive, negative
+print 'made money percentage: ', positive/period*100
+print 'lost money percentage: ', negative/period*100
+	
 t = np.arange(0,len(results),1)
 plt.bar(t, results)
 plt.title("Monte Carlo Roullette")
