8bd2232405b38614cc8ee8e95d0d0cce2a3dd6ee
Roo541 <33165603+Roo541@users.noreply.github.com>
2019-03-24 18:07:52 -0700

    changes

--- Finance_Code/Sentex.py
+++ Finance_Code/Sentex.py
@@ -31,6 +31,100 @@ def rollDice():
 		#print roll, "Win"
 		return True
 	
+def multiple_bettor(funds, initial_wager, wager_count):
+	global multiple_busts
+	global multiple_profits
+	value = funds
+	wager = initial_wager
+	wX = []
+	wY = []
+	currentWager = 1
+	previousWager = "Win"
+	
+	while currentWager <= wager_count:
+		if previousWager == "Win":
+			if rollDice():
+				value += wager
+				#print value
+				# ~ wX.append(currentWager)
+				# ~ wY.append(value)
+			else:
+				value -= wager
+				previousWager = "Loss"
+				previousWagerAmount = wager
+				# ~ wX.append(currentWager)
+				# ~ wY.append(value)
+				if value < 0:
+					multiple_busts += 1
+					break
+		elif previousWager == "Loss":
+			if rollDice():
+				wager = previousWagerAmount * random_multiple			
+				if (value - wager) < 0:
+					wager = value
+				value += wager 
+				wager = initial_wager
+				previousWager = "Win"
+				# ~ wX.append(currentWager)
+				# ~ wY.append(value)
+			else:
+				wager = previousWagerAmount * random_multiple
+				if (value - wager) < 0:
+					wager = value
+				value -= wager
+				if value < 0:
+					multiple_busts += 1
+					break
+				previousWager = "Loss"
+				previousWagerAmount = wager
+				# ~ wX.append(currentWager)
+				# ~ wY.append(value)
+		
+		currentWager += 1
+	# ~ plt.plot(wX, vY, color)
+	if value > funds:
+		multiple_profits += 1
+
+while True:
+	multiple_busts = 0.0
+	multiple_profits = 0.0 
+	multipleSampSize = 5000
+	currentSample = 1
+	random_multiple = random.uniform(0.1,5.0)
+	
+	while currentSample <= multipleSampSize:
+		multiple_bettor(startingFunds, wagerSize, wagerCount)
+		currentSample += 1
+
+	if ((multiple_busts/multipleSampSize)*100.00 < lower_bust) and ((multiple_profits/multipleSampSize)*100.00 > higher_profit):
+		print "#################################################"
+		print "Found a winner, the multiple was", random_multiple
+		print "Lower bust to beat", lower_bust
+		print "Higher profit rate to beat:", higher_profit
+		print "bust rate:", (multiple_busts/multipleSampSize)*100.00
+		print "profit rate:", (multiple_profits/multipleSampSize)*100.00
+		print "#################################################"
+			
+
+
+# ~ xx = 0
+
+# ~ while xx < sampleSize:
+	# ~ doubler_bettor(startingFunds, wagerSize, wagerCount, 'c')
+	# ~ simple_bettor(startingFunds, wagerSize, wagerCount, 'k')
+	# ~ xx += 1
+
+# ~ print "simple Bettor bust chance:", (simple_busts/sampleSize)*100.00
+# ~ print "Doubler Bettor bust chance:", (doubler_busts/sampleSize)*100.00
+
+# ~ print "Simple Bettor profit chances:", (simple_profits/sampleSize)*100.00
+# ~ print "Doubler Bettor profit cahnces:", (doubler_profits/sampleSize)*100.00
+
+# ~ plt.axhline(0, color = 'r')
+# ~ plt.ylabel("Account Value")
+# ~ plt.xlabel("Wager Count")
+# ~ plt.show()
+
 # ~ def doubler_bettor(funds, initial_wager, wager_count, color):
 	# ~ value = funds
 	# ~ wager = initial_wager
@@ -117,107 +211,3 @@ def rollDice():
 	# ~ plt.plot(wX, vY, color)
 	# ~ if value > funds:
 		# ~ simple_profits += 1
-
-def multiple_bettor(funds, initial_wager, wager_count):
-	global multiple_busts
-	global multiple_profits
-	value = funds
-	wager = initial_wager
-	wX = []
-	wY = []
-	currentWager = 1
-	previousWager = "Win"
-	
-	while currentWager <= wager_count:
-		if previousWager == "Win":
-			if rollDice():
-				value += wager
-				#print value
-				# ~ wX.append(currentWager)
-				# ~ wY.append(value)
-			else:
-				value -= wager
-				previousWager = "Loss"
-				previousWagerAmount = wager
-				# ~ wX.append(currentWager)
-				# ~ wY.append(value)
-				if value < 0:
-					multiple_busts += 1
-					break
-		elif previousWager == "Loss":
-			if rollDice():
-				wager = previousWagerAmount * random_multiple			
-				if (value - wager) < 0:
-					wager = value
-				value += wager 
-				wager = initial_wager
-				previousWager = "Win"
-				# ~ wX.append(currentWager)
-				# ~ wY.append(value)
-			else:
-				wager = previousWagerAmount * random_multiple
-				if (value - wager) < 0:
-					wager = value
-				value -= wager
-				if value < 0:
-					multiple_busts += 1
-					break
-				previousWager = "Loss"
-				previousWagerAmount = wager
-				# ~ wX.append(currentWager)
-				# ~ wY.append(value)
-		
-		currentWager += 1
-	# ~ plt.plot(wX, vY, color)
-	if value > funds:
-		multiple_profits += 1
-
-while True:
-	multiple_busts = 0.0
-	multiple_profits = 0.0 
-	multipleSampSize = 100000
-	currentSample = 1
-	random_multiple = random.uniform(1.4,1.85)
-	
-	while currentSample <= multipleSampSize:
-		multiple_bettor(startingFunds, wagerSize, wagerCount)
-		currentSample += 1
-
-	if ((multiple_busts/multipleSampSize)*100.00 < lower_bust) and ((multiple_profits/multipleSampSize)*100.00 > higher_profit):
-		print "#################################################"
-		print "Found a winner, the multiple was", random_multiple
-		print "Lower bust to beat", lower_bust
-		print "Higher profit rate to beat:", higher_profit
-		print "bust rate:", (multiple_busts/multipleSampSize)*100.00
-		print "profit rate:", (multiple_profits/multipleSampSize)*100.00
-		print "#################################################"
-		time.sleep(2)
-	
-	else:
-		print "**************************************************"
-		print "Found a loser, the multiple was", random_multiple
-		print "Lower bust to beat", lower_bust
-		print "Higher profit rate to beat:", higher_profit
-		print "bust rate:", (multiple_busts/multipleSampSize)*100.00
-		print "profit rate:", (multiple_profits/multipleSampSize)*100.00
-		print "**************************************************"
-
-# ~ xx = 0
-
-# ~ while xx < sampleSize:
-	# ~ doubler_bettor(startingFunds, wagerSize, wagerCount, 'c')
-	# ~ simple_bettor(startingFunds, wagerSize, wagerCount, 'k')
-	# ~ xx += 1
-
-# ~ print "simple Bettor bust chance:", (simple_busts/sampleSize)*100.00
-# ~ print "Doubler Bettor bust chance:", (doubler_busts/sampleSize)*100.00
-
-# ~ print "Simple Bettor profit chances:", (simple_profits/sampleSize)*100.00
-# ~ print "Doubler Bettor profit cahnces:", (doubler_profits/sampleSize)*100.00
-
-# ~ plt.axhline(0, color = 'r')
-# ~ plt.ylabel("Account Value")
-# ~ plt.xlabel("Wager Count")
-# ~ plt.show()
-
-
