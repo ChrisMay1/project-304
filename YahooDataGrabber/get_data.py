37be66dbb38ad8c1ae876438b285346a7ee2482e
Ryan Lance <lancer@oregonstate.edu>
2019-03-24 22:09:46 -0700

    deleted extra files and moar

--- YahooDataGrabber/get_data.py
+++ YahooDataGrabber/get_data.py
@@ -4,25 +4,47 @@ import numpy as np
 import pandas as pd
 import pandas_datareader.data as web
 
-start = datetime.datetime(2018, 1, 1)
-end = datetime.datetime(2019, 1, 1)
+class DataSys:
+	
+	def __init__(self, numstocks):
+		
+		if numstocks == 'all':
+			N = 30
+		else:
+			N = numstocks
+		
+		start = datetime.datetime(2018, 1, 1)
+		end = datetime.datetime(2019, 1, 1)
 
-symbols = pd.read_csv('../Finance_Code/Dow_30.csv')['Symbol']
-API = 'yahoo'
+		symbols = pd.read_csv('../Finance_Code/Dow_30.csv')['Symbol']
+		Nstocks = len(symbols)
+		API = 'yahoo'
 
-stock = web.DataReader(symbols[0], API, start, end)
+		print "Retriving", N, "stocks.."
+		stocks = []
+		available_symb = []
+		for i in range(N):
+			stocks.append(web.DataReader(symbols[i], API, start, end))
+		print "Stocks retrieved:", symbols
+		
+		self.symbols = available_symb
+		self.stocks = stocks
+		self.length = len(stocks[0])
+	
+	def show_stocks(self):
+		print self.symbols
+	
+	def retrieve_stockdata(self, stock):
+		return web.DataReader(stock, API, start, end)
 
-data = stock['Open']
+system = DataSys(3)
 
-N = len(data)
-x = np.arange(N)
-
-y = np.fft.fft(data)
-
+x = np.arange(system.length)
 
 fig = plt.figure("the stocks")
 ax1 = fig.add_subplot(111)
 
-ax1.plot(x, y)
+for i in range(3):
+	ax1.plot(x, system.stocks[i]['Open'])
 
 plt.show()
