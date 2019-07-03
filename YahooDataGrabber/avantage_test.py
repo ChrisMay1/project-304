4e5d4eb574121dce65a5c932decece4086e2694c
ChrisMay1 <maychr@oregonstate.edu>
2019-07-02 23:08:32 -0700

    Updated with parsing json and commented out bug

--- YahooDataGrabber/avantage_test.py
+++ YahooDataGrabber/avantage_test.py
@@ -1,14 +1,46 @@
 import urllib.request
+import json, os
 
 APIKEY = '8DV5XP4NBXHQO2PG'
+ticker = 'TMO'
 
 def import_web(ticker):
-	url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+ticker+'&interval=60min&apikey='+APIKEY+'&outputsize=compact&datatype=json'
+	url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+ticker+'&interval=1min&apikey='+APIKEY+'&outputsize=full&datatype=json'
 	fp = urllib.request.urlopen(url)
 	mybytes = fp.read()
 	mystr = mybytes.decode("utf8")
 	fp.close()
 	return mystr
 
-ticker = 'TMO'
-print(import_web(ticker))
+# ~ def partitionSave(ps,ticker):
+	# ~ date = {}
+	# ~ for i in ps:
+		# ~ date[i[:10]] = 'date'
+	# ~ for d in date.keys():
+		# ~ tmp = {}
+		# ~ for i in ps:
+			# ~ if (i[:10] == d):
+				# ~ tmp[i] = ps[i]
+		# ~ if(os.path.isdir(d) == False):
+			# ~ os.mkdir(d)
+		# ~ fname = ticker + "_dann"
+		# ~ try:
+			# ~ with open(os.path.join(d,fname),'r') as f:
+				# ~ t = json.load(f)
+				# ~ for i in t:
+					# ~ tmp[i] = t[i]
+		# ~ except Exception as e:
+			# ~ pass
+		# ~ with open(os.path.join(d,fname), 'w') as f:
+			# ~ json.dump(tmp,f)
+			
+
+def get_value(ticker):
+	js = import_web(ticker)
+	parsed_data = json.loads(js)
+	ps = parsed_data['Time Series (1min)']
+	print(ps)
+
+get_value('GOOGL')
+
+
